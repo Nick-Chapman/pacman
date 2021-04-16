32eb3365a07a31a6cf1b80f2e822ccefa99e9c02
Nick Chapman <nic@amadido.co.uk>
2021-04-16 23:26:50 +0100

    sprite-rom & screen display (based on dumped sprite register values)

--- src/Top.hs
+++ src/Top.hs
@@ -24,12 +24,33 @@ main = do
 
 seeScreen :: Prog Picture
 seeScreen = do
-  drawScreen
+  drawTiles
+  sequence_ [drawSprite i | i <- [0..7]]
   screen <- GetScreen
   pure $ pictureScreen screen
 
-drawScreen :: Prog ()
-drawScreen = do
+drawSprite :: Int -> Prog ()
+drawSprite i = do
+  xLoc <- ReadMem (0x5060 + 2 * fromIntegral i)
+  yLoc <- ReadMem (0x5061 + 2 * fromIntegral i)
+  info <- ReadMem (0x4ff0 + 2 * fromIntegral i)
+  palb <- ReadMem (0x4ff1 + 2 * fromIntegral i)
+  let yFlip = unByte info `testBit` 0
+  let xFlip = unByte info `testBit` 1
+  let spriteIndex = fromIntegral (info `div` 4)
+  let x0 = fromIntegral (28*8 + 15 - xLoc)
+  let y0 = fromIntegral (32*8 + 16 - yLoc)
+  palette <- readPalette (makePI (fromIntegral palb))
+  sprite <- readSprite (SI spriteIndex)
+  let (Sprite piis) = if xFlip||yFlip then undefined else sprite -- TODO
+  let xys = [ XY (x+x0) (y+y0) | y <- [0..15] , x <- [0..15]]
+  sequence_ [ SetPixel xy rgb
+            | (xy,pii) <- zip xys piis
+            , let rgb = resolvePaletteItemIndex palette pii
+            ]
+
+drawTiles :: Prog ()
+drawTiles = do
   let
     width = 28
     height = 32 -- of mid-screen area
@@ -63,9 +84,29 @@ drawTile xy i = do
             , let rgb = resolvePaletteItemIndex palette pii
             ]
 
-newtype TileIndex = TI Int deriving (Num,Enum,Integral,Real,Ord,Eq)
 
-data Tile = Tile [PaletteItemIndex] -- #64
+newtype SpriteIndex = SI Int deriving (Num,Enum,Integral,Real,Ord,Eq) -- 0..63
+
+data Sprite = Sprite [PaletteItemIndex] -- #(16x16)
+
+readSprite :: SpriteIndex -> Prog Sprite
+readSprite (SI i) = do
+  let bytesPerSprite = 64
+  let
+    readStrip off = do
+      byte <- ReadSpriteRom (bytesPerSprite*i + off)
+      pure $ decodeTileByte byte
+  layer1 <- mapM readStrip (reverse ([8..8+7] ++ [32+8+0..32+8+7]))
+  layer2 <- mapM readStrip (reverse ([16..16+7] ++ [32+16+0..32+16+7]))
+  layer3 <- mapM readStrip (reverse ([24..24+7] ++ [32+24+0..32+24+7]))
+  layer4 <- mapM readStrip (reverse ([0..7] ++ [32+0..32+7]))
+  let piis = concat [ concat (transpose layer) | layer <- [layer1,layer2,layer3,layer4] ]
+  pure $ Sprite piis
+
+
+newtype TileIndex = TI Int deriving (Num,Enum,Integral,Real,Ord,Eq) -- 0..255
+
+data Tile = Tile [PaletteItemIndex] -- #(8x8)
 
 data PaletteItemIndex = P0 | P1 | P2 | P3
 
@@ -140,6 +181,7 @@ data Prog a where
   ReadColRom :: Int -> Prog Byte
   ReadPalRom :: Int -> Prog Byte
   ReadTileRom :: Int -> Prog Byte
+  ReadSpriteRom :: Int -> Prog Byte
   ReadMem :: Addr -> Prog Byte
   SetPixel :: XY -> RGB -> Prog ()
   GetScreen :: Prog Screen
@@ -152,23 +194,64 @@ data Machine = Machine
   { colRom :: Rom
   , palRom :: Rom
   , tileRom :: Rom
+  , spriteRom :: Rom
   , mem :: Mem
   }
 
 data Mem = Mem { dump :: Rom }
 
 readMem :: Mem -> Addr -> Byte
-readMem Mem{dump} addr =
-  Rom.lookup dump (fromIntegral (addr - baseVideoRam))
+readMem Mem{dump} a =
+  case Map.lookup a spriteAddrDump of
+    Just b -> b
+    Nothing -> Rom.lookup dump (fromIntegral (a - baseVideoRam))
+
+
+spriteAddrDump :: Map Addr Byte
+spriteAddrDump = Map.fromList
+  [ (0x4ff0, 0x00)
+  , (0x4ff1, 0x00)
+  , (0x4ff2, 0x94)
+  , (0x4ff3, 0x01)
+  , (0x4ff4, 0x94)
+  , (0x4ff5, 0x03)
+  , (0x4ff6, 0x8c)
+  , (0x4ff7, 0x05)
+  , (0x4ff8, 0x8c)
+  , (0x4ff9, 0x07)
+  , (0x4ffa, 0xb8)
+  , (0x4ffb, 0x09)
+  , (0x4ffc, 0xfc)
+  , (0x4ffd, 0x00)
+  , (0x4ffe, 0x00)
+  , (0x4fff, 0x00)
+  , (0x5060, 0x00)
+  , (0x5061, 0x00)
+  , (0x5062, 0xb6)
+  , (0x5063, 0x8c)
+  , (0x5064, 0xa1)
+  , (0x5065, 0xa4)
+  , (0x5066, 0x97)
+  , (0x5067, 0x8e)
+  , (0x5068, 0x77)
+  , (0x5069, 0x8e)
+  , (0x506a, 0xa3)
+  , (0x506b, 0x2c)
+  , (0x506c, 0x07)
+  , (0x506d, 0x08)
+  , (0x506e, 0x00)
+  , (0x506f, 0x00)
+  ]
 
 initMachine :: IO Machine
 initMachine = do
   colRom <- Rom.load 32 "roms/82s123.7f"
   palRom <- Rom.load 256 "roms/82s126.4a"
   tileRom <- Rom.load 4096 "roms/pacman.5e"
+  spriteRom <- Rom.load 4096 "roms/pacman.5f"
   dump <- Rom.load 2048 "dump"
   let mem = Mem { dump }
-  pure $ Machine { colRom, palRom, tileRom, mem } where
+  pure $ Machine { colRom, palRom, tileRom, spriteRom, mem } where
 
 data Screen = Screen { m :: Map XY RGB }
 
@@ -191,7 +274,7 @@ state0 :: State
 state0 = State { screen = screen0 }
 
 run :: Machine -> Prog a -> IO a
-run Machine{colRom,palRom,tileRom,mem} prog0 = eval prog0 state0 (\_ -> pure)
+run Machine{colRom,palRom,tileRom,spriteRom,mem} prog0 = eval prog0 state0 (\_ -> pure)
   where
   eval :: Prog b -> State -> (State -> b -> IO a) -> IO a
   eval prog s k = case prog of
@@ -201,6 +284,7 @@ run Machine{colRom,palRom,tileRom,mem} prog0 = eval prog0 state0 (\_ -> pure)
     ReadColRom i -> k s (Rom.lookup colRom i)
     ReadPalRom i -> k s (Rom.lookup palRom i)
     ReadTileRom i -> k s (Rom.lookup tileRom i)
+    ReadSpriteRom i -> k s (Rom.lookup spriteRom i)
     ReadMem a -> k s (readMem mem a)
     SetPixel xy rgb -> k s { screen = setScreenPixel (screen s) xy rgb } ()
     GetScreen -> k s (screen s)
